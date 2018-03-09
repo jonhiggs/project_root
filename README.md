b52350dbdbb6df9f4e3dc5cbb8607f0353f1bbe5
Jon Higgs <jhiggs@eml.cc>
2018-03-09 14:21:31 +1100

    First release

---
+++ README.md
@@ -0,0 +1,14 @@
+# project_root
+
+Find the root of a project.
+
+It will traverse up the directory tree until it finds and returns the path which contains a `.git` directory.
+
+## Usage
+
+`project_root <path>`
+
+
+## Build
+
+`cargo build --release`
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,56 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use std::env;
+use std::fs;
+use std::process;
+use std::path::PathBuf;
+
+fn collect_parents(path: std::path::PathBuf, paths: &mut Vec<PathBuf>) -> &Vec<PathBuf> {
+    &paths.push(PathBuf::from(path));
+
+    loop {
+        let parent = PathBuf::from(paths.last().unwrap().parent().unwrap());
+        &paths.push(parent);
+        if paths.last().unwrap() == &PathBuf::from("/") {
+            break;
+        }
     }
+
+    return paths;
+}
+
+fn has_dotgit(path: &std::path::PathBuf) -> bool {
+    if path.join(".git").exists() {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("You must provide a directory as an argument");
+        process::exit(1);
+    }
+
+    let path_string = fs::canonicalize(&args[1]).unwrap();
+    let path = PathBuf::from(path_string);
+
+    if !path.exists() {
+        println!("Invalid path {:?}", path);
+        process::exit(1);
+    }
+
+    let mut paths: Vec<PathBuf> = Vec::new();
+    collect_parents(path, &mut paths);
+
+    for path in &paths {
+        if has_dotgit(&path) {
+            println!("{}", path.display());
+            process::exit(0);
+        }
+    }
+
+    process::exit(1);
 }
