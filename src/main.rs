25876a7bf150d6a6cd3370a0177424915d881988
Jon Higgs <jhiggs@eml.cc>
2018-03-13 16:54:04 +1100

    Add getopts

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+extern crate getopts;
+
+use getopts::Options;
 use std::env;
 use std::fs;
-use std::process;
 use std::path::PathBuf;
+use std::process;
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options] FILE", program);
+    print!("{}", opts.usage(&brief));
+}
 
 fn collect_parents(path: std::path::PathBuf, paths: &mut Vec<PathBuf>) -> &Vec<PathBuf> {
     &paths.push(PathBuf::from(path));
@@ -28,13 +36,37 @@ fn has_dotgit(path: &std::path::PathBuf) -> bool {
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
 
-    if args.len() != 2 {
-        println!("You must provide a directory as an argument");
-        process::exit(1);
+    let mut opts = Options::new();
+    opts.optflag("v", "verbose", "turn on verbose logging");
+    opts.optflag("h", "help", "print this help menu");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
     }
 
-    let path_string = fs::canonicalize(&args[1]).unwrap();
+    let verbose = matches.opt_present("v");
+    let path_arg = args.last().unwrap();
+
+    // let input = if !matches.free.is_empty() {
+    //     matches.free[0].clone()
+    // } else {
+    //     print_usage(&program, opts);
+    //     return;
+    // };
+    //do_work(&input, output);
+
+    if verbose {
+        println!("args are {:?}", &args);
+        println!("path_arg is {:?}", &path_arg);
+    }
+
+    let path_string = fs::canonicalize(path_arg).unwrap_or("/");
     let path = PathBuf::from(path_string);
 
     if !path.exists() {
